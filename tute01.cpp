434ca5ba0e862f994e58d92ef47ea78492c000ea
IT21235238 <99632629+IT21235238@users.noreply.github.com>
2022-02-15 07:32:29 +0000

    tutorial 02,   tute 03

--- tute01.cpp
+++ tute01.cpp
@@ -24,8 +24,8 @@ int main()
 {
     float cm, inches;
     cout<<"Enter a length in cm : ";
-    cin>>cm;
+    cin>>cm; 
     inches = cm / 2.54;
-    cout<<"Length in inches is "<< inches <<endl;
-    return 0;
+    cout<<"Length in inches is  "<< inches << endl;
+     return 0;
 }  
