4431a52971786d40ffb1f7d970bea417cb1224ef
IT21235238 <99632629+IT21235238@users.noreply.github.com>
2022-02-15 07:25:52 +0000

    tutorial 02 ,  tute o1

--- tute02.cpp
+++ tute02.cpp
@@ -12,19 +12,17 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <iostream>
-using namespace std;
-
+#include <stdio.h>
 int main()
 {
    double salary, netSalary;
    int etype, otHrs, otRate;
-   cout<<"Enter Employee Type : ";
-   cin>>etype;
-   cout<<"Enter Salary  : ";
-   cin>>salary;
-   cout<<"Enter OtHrs : ";
-   cin>>otHrs; 
+   printf("Enter Employee Type : ");
+   scanf("%d", &etype);
+   printf("Enter Salary  : ");
+   scanf("%f", &salary);
+   printf("Enter OtHrs : ");
+   scanf("%d", &otHrs); 
    
    switch (etype) {
       case 1 :
@@ -40,7 +38,7 @@ int main()
 
 
    netSalary = salary + otHrs* otRate;
-   cout<<"Net Salary is  "<<netSalary << endl;
+   printf("Net Salary is %f ", netSalary);
   
    return 0;
 }
--- tute03.cpp
+++ tute03.cpp
@@ -5,20 +5,19 @@ Convert the C program given below which calculates the Factorial of a number tha
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
 #include <iostream>
-using namespace std;
 int main()
 {
     int no;
     long fac;
 
-    cout<<"Enter a Number : ";
-    cin >> no;
+    printf("Enter a Number : ");
+    scanf("%d", &no);
 
     fac = 1;
     for (int r=no; r >= 1; r--) {
         fac = fac * r;
     }
 
-    cout<<"Factorial of is :"<<no << fac<<endl;  
+    printf("Factorial of %d is %ld\n", no, fac);  
     return 0;
 }
